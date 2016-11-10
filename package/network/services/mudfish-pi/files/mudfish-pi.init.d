205638fd746f5aacd26fcace4a8bd82bdc86d167
Chloe Jeong <chloe0528@nate.com>
2016-11-10 17:43:59 +0900

    .

--- package/network/services/mudfish-pi/files/mudfish-pi.init.d
+++ package/network/services/mudfish-pi/files/mudfish-pi.init.d
@@ -7,34 +7,12 @@ USE_PROCD=1
 VERSION=`cat /etc/mudfish-pi/version`
 MUDRUN_BIN="/opt/mudfish-pi/${VERSION}/bin/mudrun"
 
-append_arg() {
-        local cfg="$1"
-        local var="$2"
-        local opt="$3"
-        local def="$4"
-        local val
-
-        config_get val "$cfg" "$var"
-        [ -n "$val" -o -n "$def" ] && procd_append_param command "$opt" "${val:-$def}"
-}
-
-append_bool() {
-        local cfg="$1"
-        local var="$2"
-        local opt="$3"
-        local def="$4"
-        local val
-
-        config_get_bool val "$cfg" "$var" "$def"
-        [ "$val" = 1 ] && procd_append_param command "$opt"
-}
-
 start_instance()
 {
         procd_open_instance
         procd_set_param respawn
         procd_set_param stderr 1
-        procd_set_param command "$MUDRUN_BIN"
+        procd_set_param command "$MUDRUN_BIN" -a $(uci get network.lan.ipaddr)
         procd_close_instance
 }
 
