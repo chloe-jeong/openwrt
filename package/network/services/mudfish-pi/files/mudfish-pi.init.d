2abfebbd6aa17071bda59429e81ca045d4d790ca
Chloe Jeong <chloe0528@nate.com>
2016-11-11 19:26:56 +0900

    .

--- package/network/services/mudfish-pi/files/mudfish-pi.init.d
+++ package/network/services/mudfish-pi/files/mudfish-pi.init.d
@@ -7,8 +7,19 @@ USE_PROCD=1
 VERSION=`cat /etc/mudfish-pi/version`
 MUDRUN_BIN="/opt/mudfish-pi/${VERSION}/bin/mudrun"
 
+section_enabled() {
+	config_get_bool enabled "$1" 'enabled' 0
+	[ $enabled -gt 0 ]
+}
+
 start_instance()
 {
+	local s="$1"
+
+	section_enabled "$s" || {
+		return 1
+	}
+
         procd_open_instance
         procd_set_param respawn
         procd_set_param stderr 1
@@ -24,11 +35,5 @@ service_triggers()
 start_service() {
         config_load mudfish-pi
         config_foreach start_instance mudfish-pi
-}
-
-section_enabled() {
-	config_get_bool enable  "$1" 'enable'  0
-	config_get_bool enabled "$1" 'enabled' 0
-	[ $enable -gt 0 ] || [ $enabled -gt 0 ]
 }
 
