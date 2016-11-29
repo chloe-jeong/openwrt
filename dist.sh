4837b07713f95d642b3d8200a10dc61cf0f8246a
Chloe Jeong <chloe0528@nate.com>
2016-11-29 21:59:48 +0900

    .

---
+++ dist.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+TOPDIR=/opt/mudfish.net/htdocs/releases/openwrt
+
+scp ./bin/ar71xx/packages/base/mudfish-pi_2.1.13-5_ar71xx.ipk \
+    kt.kr.n.loxch.com:${TOPDIR}/chaos_calmer/15.05.1/ar71xx/packages/base
+
+scp ./bin/ar71xx/packages/luci/luci-app-mudfish-pi_git-16.334.11203-852cf6e-1_all.ipk \
+    kt.kr.n.loxch.com:${TOPDIR}/chaos_calmer/15.05.1/ar71xx/packages/luci
+
