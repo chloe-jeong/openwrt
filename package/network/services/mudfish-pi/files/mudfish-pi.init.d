a047f1ce89b4511c975e3555fc5d86211a3c87bf
Chloe Jeong <chloe0528@nate.com>
2016-12-01 00:49:19 +0900

    .

--- package/network/services/mudfish-pi/files/mudfish-pi.init.d
+++ package/network/services/mudfish-pi/files/mudfish-pi.init.d
@@ -17,7 +17,8 @@ start_instance()
         procd_open_instance
         procd_set_param respawn
         procd_set_param stderr 1
-        procd_set_param command "$MUDRUN_BIN" -a $(uci get network.lan.ipaddr)
+        procd_set_param command "$MUDRUN_BIN" \
+	    -P -a $(uci get network.lan.ipaddr)
         procd_close_instance
 }
 
