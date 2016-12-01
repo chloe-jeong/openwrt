6ea7820fae729f988dc5f31f03b44bd553cdff01
Chloe Jeong <chloe0528@nate.com>
2016-12-01 14:13:41 +0900

    .

--- dist.sh
+++ dist.sh
@@ -5,6 +5,6 @@ TOPDIR=/opt/mudfish.net/htdocs/releases/openwrt
 scp ./bin/ar71xx/packages/base/mudfish-pi_2.1.13-5_ar71xx.ipk \
     kt.kr.n.loxch.com:${TOPDIR}/chaos_calmer/15.05.1/ar71xx/packages/base
 
-scp ./bin/ar71xx/packages/luci/luci-app-mudfish-pi_git-16.334.11203-852cf6e-1_all.ipk \
+scp ./bin/ar71xx/packages/luci/luci-app-mudfish-pi_git-*.ipk \
     kt.kr.n.loxch.com:${TOPDIR}/chaos_calmer/15.05.1/ar71xx/packages/luci
 
