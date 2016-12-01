733435cb599efea33d7e87a3b5cf9d965dee19d4
Chloe Jeong <chloe0528@nate.com>
2016-12-02 02:46:42 +0900

    .

--- package/network/services/mudfish-pi/files/mudfish-pi.init.d
+++ package/network/services/mudfish-pi/files/mudfish-pi.init.d
@@ -8,27 +8,29 @@ VERSION=`cat /etc/mudfish-pi/version`
 MUDRUN_BIN="/opt/mudfish-pi/${VERSION}/bin/mudrun"
 
 section_enabled() {
-	config_get_bool enabled "$1" 'enabled' 0
-	[ $enabled -gt 0 ]
+    config_get_bool enabled "$1" 'enabled' 0
+    [ $enabled -gt 0 ]
 }
 
 start_instance()
 {
-        procd_open_instance
-        procd_set_param respawn
-        procd_set_param stderr 1
-        procd_set_param command "$MUDRUN_BIN" \
-	    -P -a $(uci get network.lan.ipaddr)
-        procd_close_instance
+    procd_open_instance
+    procd_set_param respawn
+    procd_set_param stderr 1
+    procd_set_param command "$MUDRUN_BIN" -P -a $(uci get network.lan.ipaddr)
+    procd_close_instance
 }
 
 service_triggers()
 {
-        procd_add_reload_trigger "mudfish-pi"
+    procd_add_reload_trigger "mudfish-pi"
 }
 
 start_service() {
-        config_load mudfish-pi
-        config_foreach start_instance mudfish-pi
+    config_load mudfish-pi
+    config_foreach start_instance mudfish-pi
 }
 
+stop_service() {
+    /etc/mudfish-pi/mudfish.fullvpn.down.sh
+}
