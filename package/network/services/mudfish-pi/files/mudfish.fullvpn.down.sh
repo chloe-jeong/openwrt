6168e0f77b837de68bced393bce24d4c37f6b529
Chloe Jeong <chloe0528@nate.com>
2016-12-24 22:00:03 +0900

    .

--- package/network/services/mudfish-pi/files/mudfish.fullvpn.down.sh
+++ package/network/services/mudfish-pi/files/mudfish.fullvpn.down.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-grep "Mudfish PI" /tmp/resolv.conf.auto.backup > /dev/null
-if [ $? = 0 ]; then
+if [ -f "/tmp/resolv.conf.auto.backup" ]; then
+    grep "Mudfish PI" /tmp/resolv.conf.auto.backup > /dev/null
+    if [ $? = 0 ]; then
 	/etc/init.d/dnsmasq restart
-else
+    else
 	/bin/cp -f /tmp/resolv.conf.auto.backup /tmp/resolv.conf.auto
 	/usr/bin/killall -HUP dnsmasq
+    fi
 fi
