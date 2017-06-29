3333a0fb6594fbdef404f0d398201f398824de9e
Chloe Jeong <chloe0528@nate.com>
2017-06-29 17:45:09 +0900

    .

--- dist.sh
+++ dist.sh
@@ -2,7 +2,7 @@
 
 TOPDIR=/opt/mudfish.net/htdocs/releases/openwrt
 
-scp ./bin/ramips/packages/base/mudfish-pi_2.1.13-5*.ipk \
+scp ./bin/ramips/packages/base/mudfish-pi_*.ipk \
     kt.kr.n.loxch.com:${TOPDIR}/snapshots/trunk/ramips/mt7628/packages/base
 
 scp ./bin/ramips/packages/luci/luci-app-mudfish-pi_git-*.ipk \
